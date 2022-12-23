dfcbdc8d55af565aa8088cfc9630f6d33ee4a82d
Juan Markus <juanmarkus98@gmail.com>
2022-12-24 00:00:53 +0700

    third commit

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-import time
-
-import redis
-from flask import Flask
+from Transaction import Transaction
+from Wallet import Wallet
+from TransactionPool import TransactionPool
+from Block import Block
+from Blockchain import Blockchain
+from BlockchainUtils import BlockchainUtils
+from AccountModel import AccountModel
+from Node import Node
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import socket
+import sys
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+cors = CORS(app)
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+@app.route("/", methods=["POST"])
+def post():
+    data = request.get_json()
+    data = jsonify(data)
+    return data
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    apiPort = int(sys.argv[3])
+    keyFile = None
+    if len(sys.argv) > 4:
+        keyFile = sys.argv[4]
+
+    node = Node(ip, port, keyFile)
+    node.startP2P()
+    node.startAPI(apiPort)
